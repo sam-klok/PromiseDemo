bdc631e7be5447a28763459236d8385eca71bc67
sam-klok <sklok11@gmail.com>
2021-08-07 21:44:45 -0700

    Initial commit

---
+++ js/promise-demo.js
@@ -0,0 +1,34 @@
+function load(url) {
+    return new Promise(function (resolve, reject) {
+        const request = new XMLHttpRequest();
+
+        request.onreadystatechange = function (e) {
+            if (this.readyState === 4) {
+                if (this.status == 200) {
+                    resolve(this.response);
+                } else {
+                    reject(this.status);
+                }
+            }
+        }
+        request.open('GET', url, true);
+        request.send();
+    });
+}
+
+const btn = document.querySelector('#btnGet');
+const msg = document.querySelector('#message');
+btn.onclick = function () {
+    load('https://www.javascripttutorial.net/sample/promise/api.json')
+        .then(
+            response => {
+                const result = JSON.parse(response);
+                msg.innerHTML = result.message;
+            },
+            error => msg.innerHTML = `Error getting the message, HTTP status: ${error}`
+        );
+}
+
+
+// C:\Repos\PromiseDemo\api.json
+// https://www.javascripttutorial.net/sample/promise/api.json
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Practical JavaScript Promise example
+
+
+Based on tutorials:
+
+https://www.javascripttutorial.net/es6/javascript-promises/
+
